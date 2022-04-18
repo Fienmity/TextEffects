b67c767132c320d53f75c2fbec505b15f04326fe
Jack Matthews <jm5112356@gmail.com>
2022-04-18 16:52:51 -0400

    Move effects into a different directory

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
-import { mockCommand } from "./mock";
-import { zalgoCommand } from "./zalgo";
-import { tinyCapsCommand } from "./tinycaps";
-import { flipCommand } from "./flip";
-import { mirrorCommand } from "./mirror";
-import { bubbleCommand } from "./bubble";
-import { bendCommand } from "./bend";
+import { mockCommand } from "./effects/mock";
+import { zalgoCommand } from "./effects/zalgo";
+import { tinyCapsCommand } from "./effects/tinycaps";
+import { flipCommand } from "./effects/flip";
+import { mirrorCommand } from "./effects/mirror";
+import { bubbleCommand } from "./effects/bubble";
+import { bendCommand } from "./effects/bend";
 
 const TextEffects: Plugin = {
   name: "TextEffects",
