d88ce6ec15636750c3f37e01ee3784f9c880ea5b
Jack Matthews <jm5112356@gmail.com>
2022-04-30 01:56:49 -0400

    Move effect imports into effects/index

---
+++ src/effects/index.ts
@@ -0,0 +1,11 @@
+import { mockCommand } from "./mock";
+import { zalgoCommand } from "./zalgo";
+import { tinyCapsCommand } from "./tinycaps";
+import { flipCommand } from "./flip";
+import { mirrorCommand } from "./mirror";
+import { bubbleCommand } from "./bubble";
+import { bendCommand } from "./bend";
+
+const effects = [mockCommand, zalgoCommand, tinyCapsCommand, flipCommand, mirrorCommand, bubbleCommand, bendCommand]
+
+export { effects };
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
 import { Plugin, registerPlugin } from "enmity-api/plugins";
-import { mockCommand } from "./effects/mock";
-import { zalgoCommand } from "./effects/zalgo";
-import { tinyCapsCommand } from "./effects/tinycaps";
-import { flipCommand } from "./effects/flip";
-import { mirrorCommand } from "./effects/mirror";
-import { bubbleCommand } from "./effects/bubble";
-import { bendCommand } from "./effects/bend";
+import { effects } from "./effects";
 
 const TextEffects: Plugin = {
   name: "TextEffects",
   commands: [],
 
   onStart() {
-    this.commands = [
-      mockCommand,
-      zalgoCommand,
-      tinyCapsCommand,
-      flipCommand,
-      mirrorCommand,
-      bubbleCommand,
-      bendCommand
-    ]
+    this.commands = effects;
   },
 
   onStop() {
